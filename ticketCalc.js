e1148db08e3d39b31ee6211ec3b7f07ac5ce45d0
Daniel Smith <danielsmith.2796@googlemail.com>
2018-05-21 10:26:11 +0100

    Functionality complete

--- ticketCalc.js
+++ ticketCalc.js
@@ -139,37 +139,77 @@ $( "#adPlus" ).click(function() {
 
 
   function totalCalc() {
-    var totalCost;
-    var cost = 0;
-    console.log(cost);
-    if (selectedDay == 2) {
-      console.log($('#adVal').val());
-      if (parseInt($('#adVal').val()) != 0){
-        cost = (parseInt(cost) + parseInt($('#adVal').val()) * parseInt(adultCost - 2));
-        console.log(cost);
+    if(selectedDay != 2){
+      var total = 0;
+      if($('#adVal').val() != 0) {
+        total = total + (parseInt($('#adVal').val()) * adultCost);
       }
-      if(parseInt($('#oapVal').val()) != 0) {
-        cost = parseInt(cost) + parseInt($('#oapVal').val()) * parseInt(oapCost - 2);
+      console.log(total);
+      if($('#oapVal').val() != 0) {
+        total = total + (parseInt($('#oapVal').val()) * oapCost);
       }
-      if(parseInt($('#studVal').val()) != 0) {
-        cost = parseInt(cost) + parseInt($('#studVal').val()) * parseInt(studentCost - 2);
+      if($('#studVal').val() != 0) {
+        total = total + (parseInt($('#studVal').val()) * studentCost);
       }
-      if(parseInt($('#child').val()) != 0) {
-        cost = cost + parseInt($('#childVal').val()) * parseInt(childVal - 2);
+      console.log(total);
+      if($('#childVal').val() != 0) {
+        total = total + (parseInt($('#childVal').val()) * childCost);
       }
-      cost = parseInt(cost);
-      console.log(cost);
-      console.log(parseInt(document.getElementById("adVal").value) * (adultCost - 2));
-      console.log(cost);
+      console.log(total);
+      console.log(total);
     }
     else {
-      cost += parseInt(document.getElementById("adVal").value) * (adultCost);
-      cost += parseInt(document.getElementById("oapVal").value) * (oapCost);
-      cost += parseInt(document.getElementById("studVal").value) * (studentCost);
-      cost += parseInt(document.getElementById("childVal").value) * (childVal);
+      var total = 0;
+      if($('#adVal').val() != 0) {
+        total = total + (parseInt($('#adVal').val()) * (adultCost - 2));
+      }
+      console.log(total);
+      if($('#oapVal').val() != 0) {
+        total = total + (parseInt($('#oapVal').val()) * (oapCost - 2));
+      }
+      if($('#studVal').val() != 0) {
+        total = total + (parseInt($('#studVal').val()) * (studentCost - 2));
+      }
+      console.log(total);
+      if($('#childVal').val() != 0) {
+        total = total + (parseInt($('#childVal').val()) * (childCost - 2));
+      }
+      console.log(total);
+      console.log(total);
     }
-    console.log("The Total cost is: £");
-    document.getElementById("total").innerHTML = cost;
+
+
+    //var totalCost;
+    //var cost = 0;
+    //console.log(cost);
+    //if (selectedDay == 2) {
+    //  console.log($('#adVal').val());
+    //  if (parseInt($('#adVal').val()) != 0){
+    //    cost = (parseInt(cost) + parseInt($('#adVal').val()) * parseInt(adultCost - 2));
+    //    console.log(cost);
+    //  }
+    //  if(parseInt($('#oapVal').val()) != 0) {
+    //    cost = parseInt(cost) + parseInt($('#oapVal').val()) * parseInt(oapCost - 2);
+    //  }
+    //  if(parseInt($('#studVal').val()) != 0) {
+    //    cost = parseInt(cost) + parseInt($('#studVal').val()) * parseInt(studentCost - 2);
+    //  }
+    //  if(parseInt($('#child').val()) != 0) {
+    //    cost = cost + parseInt($('#childVal').val()) * parseInt(childVal - 2);
+    //  }
+    //  cost = parseInt(cost);
+    //  console.log(cost);
+    //  console.log(parseInt(document.getElementById("adVal").value) * (adultCost - 2));
+    //  console.log(cost);
+    //}
+    //else {
+    //  cost += parseInt(document.getElementById("adVal").value) * (adultCost);
+    //  cost += parseInt(document.getElementById("oapVal").value) * (oapCost);
+    //  cost += parseInt(document.getElementById("studVal").value) * (studentCost);
+    //  cost += parseInt(document.getElementById("childVal").value) * (childVal);
+    //}
+    //console.log("The Total cost is: £");
+    document.getElementById("total").innerHTML = "The total cost is: £" + total;
 
   }
   //console.log(returnedJson[0].Title);
